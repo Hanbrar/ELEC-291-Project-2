e9eaf289686fc35c283e72bc169fba7a03639589
Jamie Kang <49418700+jamiekang7@users.noreply.github.com>
2025-03-27 17:13:31 -0700

    Update Servo.c

--- Servo/Servo.c
+++ Servo/Servo.c
@@ -12,178 +12,201 @@
 
 #define SYSCLK 40000000L
 #define FREQ 100000L // We need the ISR for timer 1 every 10 us
-#define Baud2BRG(desired_baud) ((SYSCLK / (16*desired_baud))-1)
+#define Baud2BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
 
-volatile int ISR_pwm1 = 150, ISR_pwm2 = 150;
-volatile int ISR_cnt = 0, ISR_frc = 0;
+volatile int ISR_pwm1=150, ISR_pwm2=150;
+volatile int ISR_pw=100, ISR_cnt=0, ISR_frc;
 
-// Generates a 1ms delay using the core timer.
+// The Interrupt Service Routine for timer 1 is used to generate one or more standard
+// hobby servo signals.  The servo signal has a fixed period of 20ms and a pulse width
+// between 0.6ms and 2.4ms.
 void wait_1ms(void)
 {
-    _CP0_SET_COUNT(0); // reset core timer
-    while (_CP0_GET_COUNT() < (SYSCLK / (2 * 1000))); // wait 1ms
+    unsigned int ui;
+    _CP0_SET_COUNT(0); // resets the core timer count
+
+    // get the core timer count
+    while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000)) );
 }
 
-// Wait for a given number of milliseconds.
 void waitms(int len)
 {
-    while(len--) wait_1ms();
+	while(len--) wait_1ms();
 }
 
-// Timer1 ISR: generates two servo PWM signals.
-// Each 10 µs tick, the ISR compares the current count with the desired pulse width.
 void __ISR(_TIMER_1_VECTOR, IPL5SOFT) Timer1_Handler(void)
 {
-    IFS0CLR = _IFS0_T1IF_MASK; // clear Timer1 interrupt flag
+	IFS0CLR=_IFS0_T1IF_MASK; // Clear timer 1 interrupt flag, bit 4 of IFS0
 
-    ISR_cnt++;
-    // PWM for servo 1 (output on RB2)
-    if (ISR_cnt < ISR_pwm1)
-        LATBbits.LATB2 = 1;
-    else
-        LATBbits.LATB2 = 0;
+	ISR_cnt++;
+	if(ISR_cnt<ISR_pwm1)
+	{
+		LATBbits.LATB2 = 1;
+	}
+	else
+	{
+		LATBbits.LATB2 = 0;
+	}
 
-    // PWM for servo 2 (output on RB5)
-    if (ISR_cnt < ISR_pwm2)
-        LATBbits.LATB5 = 1;
-    else
-        LATBbits.LATB5 = 0;
+	if(ISR_cnt<ISR_pwm2)
+	{
+		LATBbits.LATB5 = 1;
+	}
+	else
+	{
+		LATBbits.LATB5 = 0;
+	}
 
-    // 2000 counts * 10 µs = 20 ms period (standard for servos)
-    if (ISR_cnt >= 2000) {
-        ISR_cnt = 0;
-        ISR_frc++;
-    }
+	if(ISR_cnt>=2000)
+	{
+		ISR_cnt=0; // 2000 * 10us=20ms
+		ISR_frc++;
+	}
 }
 
-// Configure Timer1 to trigger every 10 µs.
-void SetupTimer1(void)
+void SetupTimer1 (void)
 {
-    __builtin_disable_interrupts();
-    PR1 = (SYSCLK / FREQ) - 1; // period so that each tick is 10 µs
-    TMR1 = 0;
-    T1CONbits.TCKPS = 0;       // prescaler 1:1
-    T1CONbits.TCS = 0;         // internal clock
-    T1CONbits.ON = 1;
-    IPC1bits.T1IP = 5;
-    IPC1bits.T1IS = 0;
-    IFS0bits.T1IF = 0;
-    IEC0bits.T1IE = 1;
-    
-    INTCONbits.MVEC = 1;       // multi-vector interrupts
-    __builtin_enable_interrupts();
+	// Explanation here: https://www.youtube.com/watch?v=bu6TTZHnMPY
+	__builtin_disable_interrupts();
+	PR1 =(SYSCLK/FREQ)-1; // since SYSCLK/FREQ = PS*(PR1+1)
+	TMR1 = 0;
+	T1CONbits.TCKPS = 0; // 3=1:256 prescale value, 2=1:64 prescale value, 1=1:8 prescale value, 0=1:1 prescale value
+	T1CONbits.TCS = 0; // Clock source
+	T1CONbits.ON = 1;
+	IPC1bits.T1IP = 5;
+	IPC1bits.T1IS = 0;
+	IFS0bits.T1IF = 0;
+	IEC0bits.T1IE = 1;
+	
+	INTCONbits.MVEC = 1; //Int multi-vector
+	__builtin_enable_interrupts();
 }
 
-// Configure UART2 for serial communication (baud rate set as needed).
 void UART2Configure(int baud_rate)
 {
-    U2RXRbits.U2RXR = 4;    // set RX to RB8
-    RPB9Rbits.RPB9R = 2;    // set TX to RB9
+    // Peripheral Pin Select
+    U2RXRbits.U2RXR = 4;    //SET RX to RB8
+    RPB9Rbits.RPB9R = 2;    //SET RB9 to TX
 
-    U2MODE = 0;            // disable autobaud; enable TX and RX, 8N1
-    U2STA = 0x1400;        // enable TX and RX
-    U2BRG = Baud2BRG(baud_rate); // set baud rate generator
-    U2MODESET = 0x8000;    // enable UART2
+    U2MODE = 0;         // disable autobaud, TX and RX enabled only, 8N1, idle=HIGH
+    U2STA = 0x1400;     // enable TX and RX
+    U2BRG = Baud2BRG(baud_rate); // U2BRG = (FPb / (16*baud)) - 1
+    
+    U2MODESET = 0x8000;     // enable UART2
 }
 
-// A simple delay using the timer ISR flag.
-void delay_ms(int msecs)
-{    
-    int ticks;
-    ISR_frc = 0;
-    ticks = msecs / 20; // because the period is 20ms
-    while(ISR_frc < ticks);
+void delay_ms (int msecs)
+{	
+	int ticks;
+	ISR_frc=0;
+	ticks=msecs/20;
+	while(ISR_frc<ticks);
 }
 
-/* SerialReceive() is a blocking function that waits for data on the UART2 RX buffer
- * and then stores all incoming data into *buffer. When a carriage return ('\r') is
- * received, a nul character is appended.
- */
+/* SerialReceive() is a blocking function that waits for data on
+ *  the UART2 RX buffer and then stores all incoming data into *buffer
+ *
+ * Note that when a carriage return '\r' is received, a nul character
+ *  is appended signifying the strings end
+ *
+ * Inputs:  *buffer = Character array/pointer to store received data into
+ *          max_size = number of bytes allocated to this pointer
+ * Outputs: Number of characters received */
 unsigned int SerialReceive(char *buffer, unsigned int max_size)
 {
     unsigned int num_char = 0;
+ 
+    /* Wait for and store incoming data until either a carriage return is received
+     *   or the number of received characters (num_chars) exceeds max_size */
     while(num_char < max_size)
     {
-        while(!U2STAbits.URXDA);   // wait until data available in RX buffer
-        *buffer = U2RXREG;         // read the received data
+        while( !U2STAbits.URXDA);   // wait until data available in RX buffer
+        *buffer = U2RXREG;          // empty contents of RX buffer into *buffer pointer
 
-        while(U2STAbits.UTXBF);    // wait while TX buffer full
-        U2TXREG = *buffer;         // echo the received character
+        while( U2STAbits.UTXBF);    // wait while TX buffer full
+        U2TXREG = *buffer;          // echo
  
-        if(*buffer == '\r')        // end of string on carriage return
+        // insert nul character to indicate end of string
+        if( *buffer == '\r')
         {
-            *buffer = '\0';
+            *buffer = '\0';     
             break;
         }
  
         buffer++;
         num_char++;
     }
+ 
     return num_char;
 }
 
-/* Pinout for DIP28 PIC32MX130 (omitted for brevity) */
+/* Pinout for DIP28 PIC32MX130:
+                                          --------
+                                   MCLR -|1     28|- AVDD 
+  VREF+/CVREF+/AN0/C3INC/RPA0/CTED1/RA0 -|2     27|- AVSS 
+        VREF-/CVREF-/AN1/RPA1/CTED2/RA1 -|3     26|- AN9/C3INA/RPB15/SCK2/CTED6/PMCS1/RB15
+   PGED1/AN2/C1IND/C2INB/C3IND/RPB0/RB0 -|4     25|- CVREFOUT/AN10/C3INB/RPB14/SCK1/CTED5/PMWR/RB14
+  PGEC1/AN3/C1INC/C2INA/RPB1/CTED12/RB1 -|5     24|- AN11/RPB13/CTPLS/PMRD/RB13
+   AN4/C1INB/C2IND/RPB2/SDA2/CTED13/RB2 -|6     23|- AN12/PMD0/RB12
+     AN5/C1INA/C2INC/RTCC/RPB3/SCL2/RB3 -|7     22|- PGEC2/TMS/RPB11/PMD1/RB11
+                                    VSS -|8     21|- PGED2/RPB10/CTED11/PMD2/RB10
+                     OSC1/CLKI/RPA2/RA2 -|9     20|- VCAP
+                OSC2/CLKO/RPA3/PMA0/RA3 -|10    19|- VSS
+                         SOSCI/RPB4/RB4 -|11    18|- TDO/RPB9/SDA1/CTED4/PMD3/RB9
+         SOSCO/RPA4/T1CK/CTED9/PMA1/RA4 -|12    17|- TCK/RPB8/SCL1/CTED10/PMD4/RB8
+                                    VDD -|13    16|- TDI/RPB7/CTED3/PMD5/INT0/RB7
+                    PGED3/RPB5/PMD7/RB5 -|14    15|- PGEC3/RPB6/PMD6/RB6
+                                          --------
+*/
 
-void main(void)
+void main (void)
 {
-    int dir1 = 1, dir2 = -1; // direction flags for oscillation (1 = increasing, -1 = decreasing)
-
-    DDPCON = 0;
+    char buf[32];
+    int pw;
+    
+	DDPCON = 0;
     CFGCON = 0;
 
-    // Configure RB2 as digital output for PWM1.
+    // Configure RB2 as digital output for PWM1
     ANSELBbits.ANSB2 = 0;   // Disable analog on RB2
     TRISBbits.TRISB2 = 0;   // Set RB2 as output
     LATBbits.LATB2 = 0;     // Initialize low
 
-    // Configure RB5 as digital output for PWM2 (digital-only pin).
-    TRISBbits.TRISB5 = 0;   // Set RB5 as output
-    LATBbits.LATB5 = 0;     // Initialize low
-    
-    SetupTimer1();           // Set Timer1 to interrupt every 10 µs
-    CFGCON = 0;
-    UART2Configure(115200);  // Configure UART2 (baud rate 115200)
+    // Configure RA2 as digital output for PWM2
+    // ANSELBbits.ANSB5 = 0;     // Turn off analog on RB5
+    TRISBbits.TRISB5 = 0;     // Set RB5 as output
+    LATBbits.LATB5 = 0;       // Initialize low
+	
+	SetupTimer1(); // Set timer 5 to interrupt every 10 us
 
-    // Allow time for the serial terminal to start.
-    delay_ms(500);
-    
-    // Clear screen and print banner.
-    printf("\x1b[2J\x1b[1;1H");
-    printf("Servo signal generator for the PIC32MX130F064B.\r\n");
-    printf("Outputs are on RB2 and RB5.\r\n");
+	CFGCON = 0;
+    UART2Configure(115200);  // Configure UART2 for a baud rate of 115200
+	
+	// Give putty a chance to start
+	delay_ms(500); // wait 500 ms
+	
+	printf("\x1b[2J\x1b[1;1H"); // Clear screen using ANSI escape sequence.
+    printf("Servo signal generator for the PIC32MX130F064B.  Output is in RB6 (pin 15).\r\n");
     printf("By Jesus Calvino-Fraga (c) 2018.\r\n");
-    printf("Pulse width oscillates between 100 (1.0ms) and 200 (2.0ms)\r\n");
-    
-    // Initialize PWM values to center position (150 ≈ 1.5ms pulse).
-    ISR_pwm1 = 150;
-    ISR_pwm2 = 150;
-    
-    // Main loop: update PWM values to create a triangular (oscillating) waveform.
-    while (1)
-    {
-        // Update servo 1 PWM (oscillates between 100 and 200)
-        ISR_pwm1 += dir1;
-        if (ISR_pwm1 >= 200) {
-            ISR_pwm1 = 200;
-            dir1 = -1;  // reverse direction to decrease
-        }
-        else if (ISR_pwm1 <= 100) {
-            ISR_pwm1 = 100;
-            dir1 = 1;   // reverse direction to increase
-        }
-
-        // Update servo 2 PWM (oscillates between 100 and 200 in opposite phase)
-        ISR_pwm2 += dir2;
-        if (ISR_pwm2 >= 200) {
-            ISR_pwm2 = 200;
-            dir2 = -1;
-        }
-        else if (ISR_pwm2 <= 100) {
-            ISR_pwm2 = 100;
-            dir2 = 1;
-        }
-
-        // Delay between each PWM update.
-        waitms(20); // 20 ms delay per step (adjust for faster/slower oscillation)
-    }
+    printf("Pulse width between 60 (for 0.6ms) and 240 (for 2.4ms)\r\n");
+	
+	while (1)
+	{
+    	if(state == 0)
+         {
+              // Move to extreme position:
+              ISR_pwm1 = 200;   // Servo 1 gets a 2.0ms pulse (assuming 10us per count)
+              ISR_pwm2 = 100;   // Servo 2 gets a 1.0ms pulse
+              waitms(2000);     // Hold for 2 seconds
+              state = 1;
+         }
+         else
+         {
+              // Return to starting position:
+              ISR_pwm1 = 100;   // Servo 1 gets a 1.0ms pulse
+              ISR_pwm2 = 200;   // Servo 2 gets a 2.0ms pulse
+              waitms(2000);     // Hold for 2 seconds
+              state = 0;
+	}
 }
+
